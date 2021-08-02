a8a5cd85c0621a2e59b3643b4d82bcb0c7ed7655
Rellik Jaeger <rellikjaeger@gmail.com>
2021-08-02 11:26:52 +0630

    GitHub OAuth2 Login added.
    Signed-off-by: Rellik Jaeger <rellikjaeger@gmail.com>

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// Create an OAuth App
+const githubClientId = '8701934e3ba550063a4c';
+const githubClientSecret = '4b738b774bc981f38b9258440f6caa9d0ec27512';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
